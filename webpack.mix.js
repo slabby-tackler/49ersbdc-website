96b6eb7c1b93945341ad4ca8d4cce40f34b1aa0b
Nicholas Wade <nwade514@gmail.com>
2017-08-22 17:17:28 -0700

    copy logos over with webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .copy('resources/assets/images/49ersbdc_logo.ico', 'public/faviocon.ico')
+   .copy('resources/assets/images/49ersbdc_new_logo_2_text.png', 'public/images/logo.png')
+   .copy('resources/assets/images/49ersbdc_new_logo_2_no_text.png', 'public/images/logo_phone.png');
